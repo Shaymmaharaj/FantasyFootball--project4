ac432cc65f94b38b057d11ddf1ff18be6c93cf2d
shyam maharaj <shyammaharaj@shyams-mbp.lan>
2019-11-19 09:53:53 -0800

    Updates

---
+++ precache-manifest.1bc8e1250e8c32432293ff8188ea5b2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b728bf9becb30e3a9301bd36f0d0e0ed",
+    "url": "/fantasy-football/index.html"
+  },
+  {
+    "revision": "bdae18edf9b7332e080a",
+    "url": "/fantasy-football/static/css/main.1a09cf9c.chunk.css"
+  },
+  {
+    "revision": "ebfd92ffe0c9dee53925",
+    "url": "/fantasy-football/static/js/2.0125d91d.chunk.js"
+  },
+  {
+    "revision": "bdae18edf9b7332e080a",
+    "url": "/fantasy-football/static/js/main.ff91b6f4.chunk.js"
+  },
+  {
+    "revision": "31e5808efe2252d339e2",
+    "url": "/fantasy-football/static/js/runtime-main.a532c1bd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/fantasy-football/static/media/logo.25bf045c.svg"
+  }
+]);
